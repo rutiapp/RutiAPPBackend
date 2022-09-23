6e08f6e9c0fb8b0d47f63a8b3ab1f6b8c0117ba9
kaixe455 <48770955+kaixe455@users.noreply.github.com>
2022-09-23 14:07:44 +0200

    Captcha system in backend verification
    Captcha system in backend verification for sign up, need to create env

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -4,14 +4,14 @@ module.exports = function(app) {
   app.use(function(req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
+      "x-access-token, x-captcha-token, Origin, Content-Type, Accept"
     );
     next();
   });
   app.post(
     "/api/auth/signup",
     [
-      verifySignUp.checkDuplicateUsernameOrEmail
+      verifySignUp.checkDuplicateUsernameOrEmail, verifySignUp.checkCaptcha
     ],
     authController.signup
   );
